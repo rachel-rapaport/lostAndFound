f3049000580e6240de8a97368cd2f1c4070ce903
Shira Biton <shirabiton387@gmail.com>
2024-11-26 11:55:49 +0200

    feat (db): create models and estabish MongoDB connection

---
+++ src/app/lib/models/foundItem.ts
@@ -0,0 +1,18 @@
+import { FoundItem } from "@/app/types/FoundItem";
+import mongoose, { Model, Schema } from "mongoose"
+
+const FoundItemSchema: Schema<FoundItem> = new Schema({
+    category: { type: String, required: true },
+    subCategory: { type: String, required: true },
+    color: { type: String, required: true },
+    city1: { type: String, required: true },
+    city2: { type: String },
+    image: { type: String, required: true },
+    description: { type: String, required: true },
+});
+
+const FoundItemModel: Model<FoundItem> =
+    mongoose.models.FoundItem ||
+    mongoose.model<FoundItem>("FoundItem", FoundItemSchema);
+
+export default FoundItemModel;
---
+++ src/app/lib/models/lostItem.ts
@@ -0,0 +1,16 @@
+import { LostItem } from "@/app/types/LostItem";
+import mongoose, { Model, Schema } from "mongoose";
+
+const LostItemSchema: Schema<LostItem> = new Schema({
+    category: { type: String, required: true },
+    subCategory: { type: String, required: true },
+    color: { type: String, required: true },
+    city1: { type: String, required: true },
+    city2: { type: String },
+});
+
+const LostItemModel: Model<LostItem> =
+    mongoose.models.LostItem ||
+    mongoose.model<LostItem>("LostItem", LostItemSchema);
+
+export default LostItemModel;
