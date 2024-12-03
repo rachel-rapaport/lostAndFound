77a79f11ab26a7fc4c2f6d28a4a4c8f0040b7b7f
rachel-rapaport <rachelport283@gmail.com>
2024-12-03 11:08:32 +0200

    fix5

--- src/app/api/get-and-filter-lostItem/route.ts
+++ src/app/api/get-and-filter-lostItem/route.ts
@@ -1,7 +1,7 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 
-export async function GET(request: NextRequest) {
+export async function GET() {
     try {
         // כאן תוכל להוסיף את הקוד שלך לטיפול בבקשת GET
         return NextResponse.json({ message: "GET request successful" });
