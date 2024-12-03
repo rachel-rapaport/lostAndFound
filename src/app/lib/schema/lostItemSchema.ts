1e8269263c7fb6c5404c1a0f619f25b50c70bd0d
rachel-rapaport <148284940+rachel-rapaport@users.noreply.github.com>
2024-12-03 11:14:02 +0200

    Update lostItemSchema.ts

--- src/app/lib/schema/lostItemSchema.ts
+++ src/app/lib/schema/lostItemSchema.ts
@@ -6,7 +6,6 @@ const LostItemSchema = new mongoose.Schema({
     subCategory: { type: String, required: true },
     userId: { type: String, required: true },
     colorId: { type: String, required: true },
-    userId:{ type: String, required: true },
     circles: { type: [CircleSchema] },
     publicTransport: { type: PublicTransportSchema },
 });
