09885a8e654de5dfa5f1f975fb31c0f26527e301
Avigail Shklovsky <9013825@gmail.com>
2024-12-08 14:13:31 +0200

    fix 2

--- src/app/types/schema/lostItemForSchema.ts
+++
@@ -1,7 +0,0 @@
-import { Document } from "mongoose";
-import { LostItem } from "../lostItem";
-
-export interface LostItemForSchema extends Document {
-  lostItem: LostItem;
-}
-
--- src/app/types/schema/notificationForSchema.ts
+++
@@ -1,7 +0,0 @@
-import { Document } from "mongoose";
-import { Notification } from "../alert";
-
-export interface NotificationForSchema extends Document {
-    notification : Notification
-} 
-
