7567306dde875cf4de017c322f4d22048897b26d
Shira Biton <shirabiton387@gmail.com>
2024-12-11 13:38:42 +0200

    fix(page.ts): remove calling to undefined EmailToUser.tsx component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-import EmailToUser from "./components/EmailToUser";
-// import Homepage from "./components/Homepage";
+import Homepage from "./components/Homepage";
 
 export default function Home() {
  
   return (
     <div className="p-[20px]">
-      {/* <Homepage /> */}
-      <EmailToUser />
+      <Homepage />
     </div>
   );
 }
