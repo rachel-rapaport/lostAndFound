9aa3cab1ef20304fd19ee0994c5d520ebdf5d21e
rachel-rapaport <rachelport283@gmail.com>
2024-12-11 09:37:52 +0200

    fix link

--- src/app/services/chat/chatService.ts
+++ src/app/services/chat/chatService.ts
@@ -2,19 +2,19 @@ import { User } from '@/app/types/props/user';
 import { createChatRoom } from './chatFirebase';
 import axios from 'axios';
 
-const baseUrl = process.env.BASE_URL
+
 
 //initiateChat and send email
 export const initiateChat = async (currentUser: User, otherUser: User) => {
 
   const roomId = await createChatRoom(String(currentUser._id), String(otherUser._id));
 
-  const chatRoomLink = `${baseUrl}/chat/${roomId}`;
+  const chatRoomLink = `${process.env.NEXT_PUBLIC_BASE_URL}/chat/${roomId}`;
 
   
   //SEND EMAIL
   const res = await axios.post("/api/send-email",{
-    to: "shirabiton387@gmail.com",
+    to: "9013825@gmail.com",
     subject: "Test email sending",
     text: `linl to chat ${chatRoomLink}`
 })
