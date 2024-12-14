75f0471fe07e260567460c26b5242bacd4899859
rachel-rapaport <rachelport283@gmail.com>
2024-12-15 01:07:54 +0200

    fix

--- src/app/lib/firebase/firebaseConfig.ts
+++ src/app/lib/firebase/firebaseConfig.ts
@@ -1,14 +1,14 @@
 import { initializeApp } from 'firebase/app'; 
-import { getDatabase, ref, set, get, onValue,push} from 'firebase/database'; 
+import { getDatabase, ref, set, get, onValue, push } from 'firebase/database'; 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBrallPgsmGCuuYyMtALMLlKP6-t5j80go", 
-    authDomain: "chat-a72e1.firebaseapp.com", 
-    databaseURL: "https://chat-a72e1-default-rtdb.europe-west1.firebasedatabase.app", 
-    projectId: "chat-a72e1", 
-    storageBucket: "chat-a72e1.firebasestorage.app", 
-    messagingSenderId: "286038223028", 
-    appId: "1:286038223028:web:086128ebc9490ffdb54fe1", 
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY, 
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN, 
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL, 
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID, 
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET, 
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID, 
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID, 
 };
 
 // Initialize the Firebase app with the given configuration.
@@ -17,5 +17,4 @@ const app = initializeApp(firebaseConfig);
 // Connect to the Firebase Realtime Database.
 const database = getDatabase(app);
 
-export { database, ref, set, get, onValue ,push}; 
-
+export { database, ref, set, get, onValue, push };
