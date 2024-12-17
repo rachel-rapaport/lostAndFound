a9ca4dfd405b12c3206925668d8a943cb7128eb6
Shira Biton <shirabiton387@gmail.com>
2024-12-17 15:28:07 +0200

    feat (tailwind): change secondary color to black and remove dark color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
-import Homepage from "./components/Homepage";
+// import Homepage from "./components/Homepage";
+// import ShowQuestions from "./components/ShowQuestions";
 
 export default function Home() {
 
   return (
     <div className="p-[20px]">
       hello
-      <Homepage />
+      {/* <Homepage /> */}
+      {/* <ShowQuestions/> */}
+      <button className="primary-btn">button</button>
+      <button className="secondary-btn">button</button>
+      <button className="delete-btn">button</button>
+      <button className="disabled-btn">button</button>
+
     </div>
   );
 }
