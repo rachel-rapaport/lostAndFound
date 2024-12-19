d68f7efcada82ea14051f38552338a61c46dd445
Avigail Shklovsky <9013825@gmail.com>
2024-12-19 14:28:05 +0200

    found items

--- src/app/components/admin-components/AdminDashboard.tsx
+++ src/app/components/admin-components/AdminDashboard.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import { UsersTable } from "./crud-tables-admin/UsersTable";
 import CategorysTable from "./crud-tables-admin/CategoriesTable";
+import LostItemsTable from "./crud-tables-admin/LostItemsTable";
 
 export const AdminDashboard = () => {
   const [activeTable, setActiveTable] = useState("users");
@@ -12,6 +13,8 @@ export const AdminDashboard = () => {
         return <UsersTable />;
       case "categories":
         return <CategorysTable />;
+      case "Lost items":
+        return <LostItemsTable />;
       default:
         return null;
     }
@@ -36,9 +39,26 @@ export const AdminDashboard = () => {
               <button
                 onClick={() => setActiveTable("categories")}
                 className={`block w-full text-right text-lg font-semibold p-2 rounded 
-                ${activeTable === "categories" ? "bg-[#515748]" : "hover:bg-[#515748]"}`}
+                ${
+                  activeTable === "categories"
+                    ? "bg-[#515748]"
+                    : "hover:bg-[#515748]"
+                }`}
               >
                 ניהול קטגוריות
+              </button>
+            </li>
+            <li>
+              <button
+                onClick={() => setActiveTable("Lost items")}
+                className={`block w-full text-right text-lg font-semibold p-2 rounded 
+                ${
+                  activeTable === "Lost items"
+                    ? "bg-title"
+                    : "hover:bg-[#515748]"
+                }`}
+              >
+                ניהול חפצים שאבדו{" "}
               </button>
             </li>
           </ul>
@@ -46,9 +66,7 @@ export const AdminDashboard = () => {
       </aside>
 
       {/* Main Content (Right Side) */}
-      <div className="flex-1 bg-white shadow p-6">
-        {renderTable()}
-      </div>
+      <div className="flex-1 bg-white shadow p-6">{renderTable()}</div>
     </div>
   );
 };
--- src/app/services/api/lostItemsService.ts
+++ src/app/services/api/lostItemsService.ts
@@ -29,7 +29,7 @@ export const getLostItemById = async (id: string) => {
 };
 
 // create new lost item
-export const createLostI = async (lostItem: LostItem) => {
+export const createLostItem = async (lostItem: LostItem) => {
   try {
     const response = await axios.post("/api/lostItem", lostItem);
     return response.data;
