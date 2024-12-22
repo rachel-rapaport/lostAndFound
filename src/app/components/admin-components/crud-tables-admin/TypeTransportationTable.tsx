28e9ac4af2a0d3de116035d54faaa0f95195c903
rachel-rapaport <rachelport283@gmail.com>
2024-12-22 15:54:49 +0200

    fix

--- src/app/components/admin-components/crud-tables-admin/TypeTransportationTable.tsx
+++ src/app/components/admin-components/crud-tables-admin/TypeTransportationTable.tsx
@@ -1,7 +1,5 @@
-import {
-  createTypePublicTransportation,
-  getTypePublicTransportations,
-} from "@/app/services/api/publicTransportationService";
+
+import { createTypePublicTransportation, getTypePublicTransportations } from "@/app/services/api/typePublicTransportationService";
 import { TypePublicTransport } from "@/app/types/props/typePublicTransport";
 import { Types } from "mongoose";
 import React, { useEffect, useState } from "react";
