056dd0837a67b844d83eb6e107e8bbdfe71058ef
Avigail Shklovsky <9013825@gmail.com>
2024-12-23 10:38:07 +0200

    fix 3

--- src/app/store/foundItemStore.ts
+++ src/app/store/foundItemStore.ts
@@ -1,7 +1,6 @@
 import { create } from "zustand";
 import { FoundItemStore } from "../types/store/foundItemStore";
-import { FoundItem } from "../types/props/foundItem";
-import { Types } from "mongoose";
+
 
 export const useFoundItemStore = create<FoundItemStore>((set) => ({
   // currentFoundItem: null,
