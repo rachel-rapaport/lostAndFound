a6e83ca7873553d5b846e4180c1d60e0b968403c
rachel-rapaport <rachelport283@gmail.com>
2024-12-24 11:16:41 +0200

    cardFilter4

--- src/app/services/api/matchService.ts
+++ src/app/services/api/matchService.ts
@@ -2,6 +2,8 @@ import { LostItem } from "@/app/types/props/lostItem";
 import axios from "axios"
 
 export const matchLostFound =  async (lostItem: LostItem)=>{
+    console.log("in server",lostItem);
+    
     try{
         const response = await axios.post("/api/match/lost-found",lostItem)
         console.log(response.data.data);
