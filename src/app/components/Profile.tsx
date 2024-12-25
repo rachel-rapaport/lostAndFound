9365dc551e01fdb3db15cf8f803480229c8b4d9c
Avigail Shklovsky <9013825@gmail.com>
2024-12-25 14:10:07 +0200

    upload image to cloudinary

--- src/app/components/Profile.tsx
+++ src/app/components/Profile.tsx
@@ -59,7 +59,7 @@ export const Profile = () => {
           </span>
         ) : (
           <Image
-            src="https://media.istockphoto.com/id/1495088043/vector/user-profile-icon-avatar-or-person-icon-profile-picture-portrait-symbol-default-portrait.jpg?s=612x612&w=0&k=20&c=dhV2p1JwmloBTOaGAtaA3AW1KSnjsdMt7-U_3EZElZ0="
+            src="https://res.cloudinary.com/dcsowksj2/image/upload/v1735128218/user-profile-icon-avatar-or-person-icon-profile-picture-portrait-symbol-default-portrait_va7pdc.jpg"
             alt="profile"
             width={50}
             height={50}
