a7cfcdf2ac68820312f94640c1809dd3f55da723
Shira Biton <shirabiton387@gmail.com>
2025-01-02 14:04:20 +0200

    fix (show questions): add get by id api request from show questions if there is no current found item

--- src/app/services/api/foundItemsService.ts
+++ src/app/services/api/foundItemsService.ts
@@ -1,10 +1,10 @@
-import { FoundItem } from "../../types/props/foundItem";
 import axios from "axios";
+import { FoundItem } from "../../types/props/foundItem";
 
 // get all found items
 export const getFoundItems = async () => {
   try {
-    const response = await axios.get("/api/foundItem",{
+    const response = await axios.get("/api/foundItem", {
       withCredentials: true,
     });
     console.log(response);
@@ -14,12 +14,6 @@ export const getFoundItems = async () => {
     throw error;
   }
 };
-// export const getFoundItems = async () => {
-//   return await FoundItemModel.find()
-//     .populate("userId")
-//     .populate("publicTransport")
-//     .populate("colorId");
-// };
 
 // get found item by id
 export const getFoundItemById = async (id: string) => {
@@ -32,11 +26,6 @@ export const getFoundItemById = async (id: string) => {
     throw error;
   }
 };
-// export const getFoundItemById = async (id: string) => {
-//   return await FoundItemModel.findById(id)
-//     .populate("circles")
-//     .populate("publicTransport");
-// };
 
 // create new found item
 export const createFoundItem = async (foundItem: FoundItem) => {
@@ -48,20 +37,6 @@ export const createFoundItem = async (foundItem: FoundItem) => {
     throw error;
   }
 };
-// export const createFoundItem = async (foundItemData: FoundItem) => {
-//   const { publicTransport, ...foundItemRest } = foundItemData;
-//   const publicTransportData = await TypePublicTransportModel.create(
-//     publicTransport
-//   );
-
-//   const foundItem = new FoundItemModel({
-//     ...foundItemRest,
-//     publicTransport: publicTransportData._id,
-//   });
-
-//   return await foundItem.save();
-// };
-
 
 // update found item by id
 export const updateFoundItemById = async (id: string, foundItem: FoundItem) => {
@@ -73,34 +48,6 @@ export const updateFoundItemById = async (id: string, foundItem: FoundItem) => {
     throw error;
   }
 };
-// export const updateFoundItemById = async (
-//   id: string,
-//   updateData: FoundItem
-// ) => {
-//   const { publicTransport, ...foundItemRest } = updateData;
-//   let publicTransportData = undefined;
-
-//   if (publicTransport) {
-//     publicTransportData = await TypePublicTransportModel.create(
-//       publicTransport
-//     );
-//   }
-
-//   const updatedFoundItem = await FoundItemModel.findByIdAndUpdate(
-//     id,
-//     {
-//       ...foundItemRest,
-//       publicTransport: publicTransportData
-//         ? publicTransportData._id
-//         : undefined,
-//     },
-//     { new: true }
-//   )
-//     .populate("circles")
-//     .populate("publicTransport");
-
-//   return updatedFoundItem;
-// };
 
 // delete found item by id
 export const deleteFoundItemById = async (id: string) => {
@@ -112,12 +59,3 @@ export const deleteFoundItemById = async (id: string) => {
     throw error;
   }
 };
-
-// export const deleteFoundItemById = async (id: string) => {
-//   const deleteFoundItem = await FoundItemModel.findByIdAndDelete(id);
-//   if (!deleteFoundItem) {
-//     return null;
-//   }
-
-//   return deleteFoundItem;
-// };
