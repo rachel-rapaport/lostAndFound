9e7e1a552a52693fd9418329c93a07a142cfda84
rachel-rapaport <rachelport283@gmail.com>
2025-01-04 22:45:54 +0200

    footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
 import Header from "./components/layOut/Header";
 import Script from "next/script";
+import Footer from "./components/layOut/Footer";
 
 const LayoutContent: React.FC<{ children: React.ReactNode }> = ({
   children,
@@ -29,6 +30,7 @@ export default function RootLayout({
       <LayoutContent>
         <Header />
         <div className="inner-body">{children}</div>
+        <Footer/>
       </LayoutContent>
     </html>
   );
