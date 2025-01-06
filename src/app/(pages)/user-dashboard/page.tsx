fd6f537cab313017c2962b595497331cd8d8d633
Avigail Shklovsky <9013825@gmail.com>
2025-01-06 11:39:51 +0200

    fix

--- src/app/(pages)/user-dashboard/page.tsx
+++ src/app/(pages)/user-dashboard/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import UserDashboard from "@/app/components/user/UserDashboard";
+import UserDashboard from "@/app/components/user/dashboard/UserDashboard";
 
 const page = () => {
   return (
